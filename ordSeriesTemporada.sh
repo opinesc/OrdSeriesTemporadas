a607faa0536c4a6b205c023672e6e0a5c0696444
Omar Piñera Escobio <omarpinera@gmail.com>
2022-06-12 18:00:53 +0200

    Se añaden las primeras funciones.

--- ordSeriesTemporada.sh
+++ ordSeriesTemporada.sh
@@ -17,9 +17,50 @@ pwd=$(pwd)
 # https://api.themoviedb.org/3/search/movie?api_key=100502338a9609958a98bcaa27a821a4&language=es&query=alguno%20hombres%20buenos&page=1
 
 # primero poner las funciones
+isdirectory() {
+  if [ -d "$1" ]
+  then
+    # 0 = true
+    return 0 
+  else
+    # 1 = false
+    return 1
+  fi
+}
 
+get_id() {
+if [ $# -lt 1 ]; then
+  echo 'Hay que proporcionar el nombre del archivo'
+else
+	echo '1402-the-walking-dead'
+fi
+}
 
+check_folder_serie() {
+  if [ $# -lt 2 ]; then
+  echo 'Hay que proporcionar la carpeta de la serie'
+else
+  #echo "hola -> $($2 | grep '^\[' | grep '\]$')"
+  if isdirectory $1$2; then 
+    if isnamefoldercorrect $2; then echo "correcto -> $2"; else echo "cambiarlo -> $2"; fi
+  else echo "No es directorio"
+  fi
+fi
+}
 
+isnamefoldercorrect() {
+if [ $# -lt 1 ]; then
+  echo 'Hay que proporcionar Nombre de la carpeta de la serie'
+  return 1
+else
+  if [ $(echo $1 | grep '^\[' | grep '\]$') ]; then return 0 ; else return 1; fi
+fi
+}
+
+# iniciamos
+# para admitir los espacios
+IFS='
+' 
 
 
 # 1. Entrar en la carpeta de las serie-> id
