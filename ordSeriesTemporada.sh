226c899678c1fcc7d0fbcefae7418abbf0e93fb2
Omar Piñera Escobio <omarpinera@gmail.com>
2022-06-13 21:50:19 +0200

    Se añade check_dependencies

--- ordSeriesTemporada.sh
+++ ordSeriesTemporada.sh
@@ -5,7 +5,21 @@ api_key=$(grep "^[0-9A-Za-z]\+$" ./api.key)
 tmdb='https://api.themoviedb.org/3/'
 get_image='https://image.tmdb.org/t/p/original'
 pwd=$(pwd)
+dependencies="jq"
 
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+# para admitir los espacios
+IFS='
+' 
 # https://api.themoviedb.org/3/movie/245891-john-wick?api_key=100502338a9609958a98bcaa27a821a4&language=es
 
 # buscar una serie
@@ -16,7 +30,22 @@ pwd=$(pwd)
 # buscar una pelicula
 # https://api.themoviedb.org/3/search/movie?api_key=100502338a9609958a98bcaa27a821a4&language=es&query=alguno%20hombres%20buenos&page=1
 
+
+
 # primero poner las funciones
+check_dependencies(){
+tput civis; counter=0
+if [ ! "$(command -v $dependencies)" ]; then
+		echo -e "${redColour}[X]${endColour}${grayColour} $dependencies${endColour}${yellowColour} no está instalado${endColour}"; sleep 1
+			echo -e "\n${yellowColour}[i]${endColour}${grayColour} Instalando...${endColour}"; sleep 1
+			apt install $dependencies -y > /dev/null 2>&1
+			echo -e "\n${greenColour}[V]${endColour}${grayColour} $dependencies${endColour}${yellowColour} instalado${endColour}\n"; sleep 2
+			let counter+=1
+else 
+  echo -e "${greenColour}[X]${endColour}${grayColour} $dependencies${endColour}${greenColour} está instalado${endColour}"; sleep 1
+fi
+}
+
 isdirectory() {
   if [ -d "$1" ]
   then
@@ -57,12 +86,29 @@ else
 fi
 }
 
-# iniciamos
-# para admitir los espacios
-IFS='
-' 
+# iniciamos funcion inicial
+init (){
+  check_dependencies
+  if [ $# -lt 1 ]; then
+  for i in $(ls -C1)
+  do
+    echo $i
+  done
+else 
+  for i in $(ls -C1 $1)
+  do
+    echo $1$i
+    # archivo $1$i
+    #get_id $i
+    #check_folder_serie $1 $i
+  done
+fi
+}
 
-
+init $1
+# Se pone 2> /dev/null -> para que no salga nada por pantalla
+#temporada=$(curl https://api.themoviedb.org/3/tv/63174\?api_key\=100502338a9609958a98bcaa27a821a4\&language\=es\&page\=1 2> /dev/null | jq ".seasons[1].name" )
+#echo "TEMPORADA->"$temporada
 # 1. Entrar en la carpeta de las serie-> id
 # 2. si hay archivos -> buscar temporada y capítulo
 #    carpeta Temporada -> ({año}) Temporada {núm. temporada} -> si no existe crearla
